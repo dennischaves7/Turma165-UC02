a6ffc711d1d3d812c5bd0ade7586b21ac32651c8
Dennis Barbosa <dennisbarbosa998@gmail.com>
2025-10-01 21:32:39 -0300

    JS

---
+++ Aula18-JS/01/exemplo.js
@@ -0,0 +1,18 @@
+//Declarando Váriaveis
+
+var name = 'Dennis'
+
+console.log('Hello World')
+
+let idade = 25
+
+int = 10 
+
+
+let a = 1
+let b = 3
+let result = a * b
+
+let comida = prompt('Informe o nome da sua comida favorita')
+console.log(comida)
+alert(`Sua comida favorita é ${comida}`)
