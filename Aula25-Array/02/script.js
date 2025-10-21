f6ab223cc0836f9233342b8853c25a6a6f9df3c8
Dennis Barbosa <dennisbarbosa998@gmail.com>
2025-10-20 21:30:18 -0300

    react

--- Aula25-Array/02/script.js
+++ Aula25-Array/02/script.js
@@ -4,32 +4,49 @@ let lista = document.querySelector("#lista");
 let btnAdd = document.querySelector("#btnAdd");
 
 btnAdd.addEventListener("click", () => {
-    lista.textContent = ""
     if (input.value === "") {
         alert("Digite uma tarefa");
-    } else {
-        tarefas.push(input.value)
+        return;
     }
-    input.value = ""
-    
-    mostrarTarefas()
+    tarefas.push(input.value);
+    input.value = "";
+    mostrarTarefas();
 });
 
-function mostrarTarefas (){
-    tarefas.forEach((t, i)=> {
-        let li = document.createElement("li");
-        li.textContent = t
-        let editar = document.createElement("button")
-        editar.textContent = "Editar"
-         let excluir = document.createElement("button")
-        excluir.textContent = "Excluir"
-        excluir.addEventListener("click", ()=> excluir(i))
-        li.appendChild(editar)
-        li.appendChild(excluir)
-        lista.appendChild(li)
-    })
-}
- 
-function excluir(i){
+function mostrarTarefas() {
+    lista.textContent = ""; 
 
+    tarefas.forEach((t, i) => {
+        let li = document.createElement("li");
+        li.textContent = t;
+
+
+        let editar = document.createElement("button");
+        editar.textContent = "Editar";
+        editar.addEventListener("click", () => editarTarefa(i));
+
+        let excluir = document.createElement("button");
+        excluir.textContent = "Excluir";
+        excluir.addEventListener("click", () => excluir(i));
+
+        li.appendChild(editar);
+        li.appendChild(excluir);
+        lista.appendChild(li);
+    });
 }
+
+
+function excluir(i) {
+    tarefas.splice(0, 1);
+    mostrarTarefas()
+}
+
+function editarTarefa(i) {
+    input.value = tarefas[i];
+    input.focus();
+    
+    tarefas.splice(i, 1);
+    
+    mostrarTarefas();
+}
+
